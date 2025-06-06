025ed5e7c46369257fbf398ffba2666c72d334a0
Eric Liu <lzhe729@aucklanduni.ac.nz>
2025-06-06 17:54:08 +1200

    add method to check the invalidate of input and print out info of countries

--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 /** This class is the main entry point. */
 public class MapEngine {
+  private List<String> countries;
+  private List<String> adjacencies;
 
   public MapEngine() {
     // add other code here if you wan
@@ -13,12 +15,62 @@ public class MapEngine {
   /** invoked one time only when constracting the MapEngine class. */
   private void loadMap() {
 
-    List<String> countries = Utils.readCountries();
-    List<String> adjacencies = Utils.readAdjacencies();
+    this.countries = Utils.readCountries();
+    this.adjacencies = Utils.readAdjacencies();
   }
 
   /** this method is invoked when the user run the command info-country. */
-  public void showInfoCountry() {}
+  public void showInfoCountry() {
+    String country = null;
+    String continent = null;
+    String value = null;
+    String[] neighbours = null;
+    int count = 0;
+
+    MessageCli.INSERT_COUNTRY.printMessage();
+    String input = Utils.scanner.nextLine().trim();
+    if (input.isEmpty() || input == null) {
+      MessageCli.INVALID_COUNTRY.printMessage(input);
+      return;
+    }
+
+    input =
+        Utils.capitalizeFirstLetterOfEachWord(input); // Capitalize the first letter of each word
+
+    for (String line : countries) {
+      String[] parts = line.split(","); // [country, continent, value]
+      String countryName = parts[0].trim();
+      if (countryName.equals(input)) {
+        country = countryName;
+        continent = parts[1].trim();
+        value = parts[2].trim();
+        count++;
+
+        break; // Exit the loop once we find the country
+      }
+    }
+    if (count == 0) {
+      // If we reach here, the country was not found
+      MessageCli.INVALID_COUNTRY.printMessage(input);
+      return;
+    }
+    for (String line : adjacencies) {
+      String[] parts = line.split(","); // [country, neighbour1, neighbour2, ...]
+      String countryName = parts[0].trim();
+      if (countryName.equals(input)) {
+        neighbours = new String[parts.length - 1];
+        for (int i = 1; i < parts.length; i++) {
+          neighbours[i - 1] = parts[i].trim();
+        }
+        break; // Exit the loop once we find the country
+      }
+    }
+
+    // Show information about the country
+
+    MessageCli.COUNTRY_INFO.printMessage(
+        country, continent, value, "[" + String.join(", ", neighbours) + "]");
+  }
 
   /** this method is invoked when the user run the command route. */
   public void showRoute() {}
